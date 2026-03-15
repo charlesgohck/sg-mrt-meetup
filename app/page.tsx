c8d21b8a67d67f7893e70aab07ddcc6da21adcdf
Charles Goh <15852016+charlesgohck@users.noreply.github.com>
2026-03-15 16:42:11 +0800

    [STATIC] Made project static

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import MrtFinderDynamic from '@/components/MrtFinderDynamic'
-
-export default function Home() {
-  return <MrtFinderDynamic />
-}
--- components/MrtFinderDynamic.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-
-const MrtFinder = dynamic(() => import('./MrtFinder'), { ssr: false })
-
-export default function MrtFinderDynamic() {
-  return <MrtFinder />
-}
